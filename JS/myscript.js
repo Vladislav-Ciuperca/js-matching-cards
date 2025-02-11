708e6c43389123f030e9f721d2086bbe1151cd59
Vladislav-Ciuperca <ciuperca37@gmail.com>
2025-02-11 19:52:10 +0100

    emojis appaiono e scompaiono dalla griglia

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,26 +1,39 @@
 
 let arrayEmojis = [
-    "&#128190",
-    "&#128190",
-    "&#128640",
-    "&#128640",
-    "&#128642",
-    "&#128642",
-    "&#128641",
-    "&#128641",
+    "&#128190",//floppy
+    "&#128640",//spaceShip
+    "&#128642",//trenino ciuf ciuf
+    "&#128641",//copter
+    "&#128509",//liberty
+    "&#128051",//fat fish
+    "&#128012",//gipsy slug
+    "&#128013",//snake
+    "&#128022",//bacon
+    "&#128029",//bee :3
+    "&#128039",//pengwing
+    "&#128060",//panda
+    "&#129430",//T-WEX
+    "&#129437",//shifu
+    "&#129453",//seal
+    "&#127843",//shu shi
+    "&#127790",//taco
+    "&#127789",//dog hot
 ]
 
+let playingEmojis = []
 let shuffledEmojis = []
+let gameCache = []
 
 let blockedElements = []
 
-let timeoutGeneral = 500;
+let timeoutGeneral = 75;
 let clickIndex = 0
 let toMatchImage = ""
 let tempIndex = 0
 let score
 let squaresNum
 let rootNum
+let allSquares
 // let squares = document.getElementsByClassName("square");
 let playBtn = document.getElementById("play");
 let notify = document.getElementById("notify");
@@ -33,10 +46,19 @@ let diffLevels = {
     "hard": 36,
 };
 
+let activationTime = {
+    "easy": 300,
+    "medium": 330,
+    "hard": 450,
+}
+
+let growingSquares = [];
+
 //-----// click sulla difficiolat //-----//
 [...diffBtns].forEach(selectDiff => {
     selectDiff.addEventListener("click", function () {
         squaresNum = diffLevels[selectDiff.innerHTML];
+        animationTimer = activationTime[selectDiff.innerHTML]
         rootNum = Math.sqrt(squaresNum);
         [...diffBtns].forEach(element => {
             element.classList.remove("active")
@@ -53,13 +75,37 @@ playBtn.addEventListener("click", function () {
         notify.innerHTML = "SELEZIOAN UNA DIFFICOLTURA"
     }
     else {
+        gameCache = [];
+        playingEmojis = [];
+        //creo una logica che ad ogni partita, in base alla difficolta , sceglie degli emoji casuali dalla lista
+        for (let y = 0; y < (squaresNum / 2);) {
+            let emojiPicker = Math.floor(Math.random() * arrayEmojis.length)
+            if (!gameCache.includes(emojiPicker)) {
+                gameCache.push(emojiPicker)
+                playingEmojis.push(arrayEmojis[emojiPicker])
+                playingEmojis.push(arrayEmojis[emojiPicker])
+                y++
+            }
+        }
 
+        shuffledEmojis = shuffleArray(playingEmojis)
+        console.log(shuffledEmojis);
 
+        playBtn.classList.add("disabled")
+        setTimeout(() => {
+            playBtn.classList.remove("disabled")
+        }, animationTimer * 3);
+
+        growingSquares = []
+        allSquares = []
         score = 0
         tempIndex = 0
         let toMatchImage = ""
         blockedElements = []
-        shuffledEmojis = shuffleArray(arrayEmojis)
+
+        for (let z = 0; z < (squaresNum / 2); z++) {
+        }
+        // shuffledEmojis = shuffleArray(arrayEmojis)
 
         gameContainer.innerHTML = ""
 
@@ -70,35 +116,61 @@ playBtn.addEventListener("click", function () {
             gameContainer.innerHTML += `<div class="row"></div>`
         }
 
-
         // creo la fuzione che popola ogni singola row una alla votla
-
         function getGameGrid(rowIndex, start, root) {
             let rows = document.getElementsByClassName("row")
             let targetRow = rows[rowIndex]
 
             let end = start + root
 
-            console.log(end);
 
+
+            //-----// qua creo la griglia di gioco //-----//
             function getSquare() {
                 if (start < end) {
-
-                    targetRow.innerHTML += `<div class="square flexed disabled">i</div>`
-
+                    //-----// qua creo le caselle //-----//
+                    targetRow.innerHTML += `<div class="square flexed disabled" 
+                    style="width: calc(100% / ${rootNum} - 10px);">${shuffledEmojis[start - 1]}</div>`
+                    growingSquares.push(start)
                     start++
-                    console.log(start);
+
+
+                    //-----// qua animo gli squares //-----//
+                    allSquares = document.getElementsByClassName("square")
+                    setTimeout(() => {
+                        for (let n = 0; n < growingSquares.length; n++) {
+                            setTimeout(() => {
+                                allSquares[growingSquares[n] - 1].classList.add("grow")
+                            }, animationTimer)
+                        }
+                    }, timeoutGeneral);
 
                 }
                 setTimeout(getSquare, timeoutGeneral)
             }
             getSquare()
+
+
+            setTimeout(() => {
+                [...allSquares].forEach(element => {
+                    element.classList.add("appear")
+                });
+            }, 1100 + (squaresNum * 10));
+
+            setTimeout(() => {
+                [...allSquares].forEach(element => {
+                    element.classList.remove("appear")
+                });
+            }, 3000);
+
         }
+
+
 
 
         for (let x = 0; x < rootNum; x++) {
             setTimeout(() => {
-                getGameGrid(x, x + rootNum, rootNum)
+                getGameGrid(x, 1 + (x * rootNum), rootNum)
             }, x * timeoutGeneral);
         }
 
@@ -113,7 +185,7 @@ playBtn.addEventListener("click", function () {
         //     emoji = shuffledEmojis[i]
 
         //     /////////////////////////////////////////////////////////////////////////////
-        //     // gameContainer.innerHTML += `<div class="square flexed disabled">${emoji}</div>`
+        //     gameContainer.innerHTML += `<div class="square flexed disabled">${emoji}</div>`
         //     /////////////////////////////////////////////////////////////////////////////
 
 
